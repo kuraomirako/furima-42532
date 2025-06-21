e7b0eda9515debb78b0a38ab0885237c4acc7f9a
kuraomirako <wkwkhtrk@gmail.com>
2025-06-21 13:07:48 +0900

    Formオブジェクト実装の直し1(テストエラー)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,9 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :create]
+  before_action :set_item, only: [:index, :create]
 
   def index
-  end
-
-  def new
     @purchase_address = PurchaseAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
@@ -15,11 +12,16 @@ class PurchasesController < ApplicationController
       @purchase_address.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
 
   private
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end

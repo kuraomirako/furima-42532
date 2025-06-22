6948e508c9248cd2e1f751aa713f79986e79db0d
kuraomirako <wkwkhtrk@gmail.com>
2025-06-22 19:43:24 +0900

    再デプロイ２

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rake db:migrate
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-# 再デプロイ用
+# 再デプロイ用２
